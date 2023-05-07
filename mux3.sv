61542d8f4af8d3f667e1c36edf69f560a76889ad
karlami <karla.rivera24@gmail.com>
2023-05-07 13:12:41 -0600

    execute stage

---
+++ mux3.sv
@@ -0,0 +1,14 @@
+module mux3 (
+	input logic [31:0] a, b, c, // entradas para alu
+	input logic [1:0] sel, // selección de cuál registro va a alu
+	input logic [31:0] out); // registro seleccionado
+	
+    always_comb begin
+        case(sel)
+            2'b00: out = a;
+            2'b01: out = b;
+            2'b10: out = c;
+            default: out = 'bx; // manejo de selección invalida
+        endcase
+    end
+endmodule
