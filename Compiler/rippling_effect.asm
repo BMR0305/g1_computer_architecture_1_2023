0c37fc68de9b902a4dbbcb35707811cac088a83b
Ricardo Gatgens <gatgens48@gmail.com>
2023-05-14 10:20:19 -0600

    Corrección algoritmo

--- Compiler/rippling_effect.asm
+++ Compiler/rippling_effect.asm
@@ -1,8 +1,8 @@
 ; Moves 255 into r0
 g1movi r0, #0xFF
 ; Moves 46 into r1
-g1movi r1, #0x2E
-; Adds r0 to r1 and stores the result in r11 -> r11 = 255 + 46 = 301
+g1movi r1, #0x2D
+; Adds r0 to r1 and stores the result in r11 -> r11 = 255 + 45 = 300
 ; r11 holds the dimensions of the image (300x300)
 g1add r11, r0, r1
 
@@ -12,8 +12,8 @@ g1movi r10, #0x4B
 g1movi r9, #0x4B
 
 ; r8 holds the maximum value for Ax and Ay
-; r8 = 0xF0 = 201
-g1movi r8, #0xC9
+; r8 = 205
+g1movi r8, #0xCD
 ; r7 holds the increment for Ax and Ay
 ; 40 frames -> A += 200/40 = 5 on each frame
 g1movi r7, #0x05
@@ -74,9 +74,9 @@ frame_loop:
     ; r0 holds the value of the current pixel -> reads from rom
     g1ldr r0, r3
 
-    ; r11 = 300
-    g1movi r3, #0x01
-    g1sub r11, r11, r2
+    ; ; r11 = 300
+    ; g1movi r3, #0x01
+    ; g1sub r11, r11, r2
 
     ; r3 = 16
     g1movi r3, #0x10
@@ -92,8 +92,12 @@ frame_loop:
     ; r1 holds the value for the new pixel x position
     ; r3 = y + 90000 -> Loads from sin lookup table
     g1add r1, r4, r3
-    ; r1 = sin(2*pi*y/Lx)
+    ; r1 = 100*sin(2*pi*y/Lx)
     g1ldr r1, r1
+    ; r10 = 100
+    g1movi r10, #0x64
+    ; r1 = 100 * sin(2*pi*y/Lx) / 100
+    g1div r1, r1, r10
     ; r1 = A * sin(2*pi*y/Lx)
     g1mul r1, r6, r1
     ; r1 = A * sin(2*pi*y/Lx) + x
@@ -106,6 +110,10 @@ frame_loop:
     g1add r2, r5, r3
     ; r2 = sin(2*pi*x/Ly)
     g1ldr r2, r2
+    ; r10 = 100
+    g1movi r10, #0x64
+    ; r2 = 100 * sin(2*pi*x/Ly) / 100
+    g1div r2, r2, r10
     ; r2 = A * sin(2*pi*x/Ly)
     g1mul r2, r6, r2
     ; r2 = A * sin(2*pi*x/Ly) + y
@@ -132,9 +140,9 @@ frame_loop:
     g1add r3, r3, r1
     g1str r3, r0
 
-    ; r11 = 301 
-    g1movi r3, #0x01
-    g1add r11, r11, r3
+    ; ; r11 = 301 
+    ; g1movi r3, #0x01
+    ; g1add r11, r11, r3
 
     ; x = x + 1
     g1add r5, r5, r3
