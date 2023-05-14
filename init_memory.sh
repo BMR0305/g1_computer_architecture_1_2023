886f450f43b3cba1991a52a8af87e56d0cb9823a
Ricardo Gatgens <gatgens48@gmail.com>
2023-05-14 01:41:23 -0600

    Script de inicialización de memoria

---
+++ init_memory.sh
@@ -0,0 +1,3 @@
+python ./Compiler/compiler.py ./Compiler/rippling_effect.asm ./IF/ROMInstructions.mem
+python ./Compiler/load_image.py ./Compiler/image.png ./Memory/init/image.mif
+python ./seno_lookup/load_seno.py ./Memory/init/sin.mif
