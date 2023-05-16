832dd206665d1b57d3943756eb75f359298d1aac
Ricardo Gatgens <gatgens48@gmail.com>
2023-05-15 19:45:39 -0600

    algoritmo compilado

--- init_memory.sh
+++ init_memory.sh
@@ -1,8 +1,10 @@
-# python ./Compiler/compiler.py ./Compiler/rippling_effect.asm ./IF/ROMInstructions.mem
+python ./Compiler/compiler.py ./Compiler/rippling_effect.asm ./IF/ROMInstructions.mem
+python ./Compiler/compiler.py ./Compiler/rippling_effect.asm ./custom_simulation/ROMInstructions.mem
+
 # python ./Compiler/compiler.py ./Compiler/arith_instructions.txt ./custom_simulation/ROMInstructions.mem 
 # python ./Compiler/compiler.py ./Compiler/mov_instruction.txt ./custom_simulation/ROMInstructions.mem
 # python ./Compiler/compiler.py ./Compiler/memory_instructions.txt ./custom_simulation/ROMInstructions.mem
-python ./Compiler/compiler.py ./Compiler/branch_instructions.txt ./custom_simulation/ROMInstructions.mem
+# python ./Compiler/compiler.py ./Compiler/branch_instructions.txt ./custom_simulation/ROMInstructions.mem
 
 python ./Compiler/load_image.py ./Compiler/image.png ./Memory/init/image.mif
 python ./seno_lookup/load_seno.py ./Memory/init/sin.mif
